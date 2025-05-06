96b00772291bd07d9ab91926e2cf74497b15eaaf
lukebalili <balili.luke@cec.edu.ph>
2025-05-06 21:29:30 +0800

    second commit

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home():
     return 'LUKE S. BALILI BSIT SYSTEM INTEGRATION SEMI-FINALS'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
